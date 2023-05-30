79203a7b79871500beb93a6dc15b5464c9971f92
adamspicedev <adam@spicey.dev>
2023-05-30 14:55:48 +1200

    updates

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -199,7 +199,7 @@ export default function Home({ articles }) {
   return (
     <>
       <Head>
-        <title>Adam Spice - Software Developer, Gamer, Husband and A dad</title>
+        <title>Adam Spice - Software Developer, gamer, husband and a dad</title>
         <meta
           name="description"
           content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
@@ -208,7 +208,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software Developer, Gamer, Husband and A dad
+            Software Developer, gamer, husband and a dad
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I&apos;m Adam, a software developer based in Auckland, New Zealand.
